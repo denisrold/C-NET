1e53605d84cd23ad81c73bc588f2d87a15ceedc9
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-02-06 13:31:54 -0300

    Operaciones basicas c#

--- C#/modulo1/MyProgram/Program.cs
+++ C#/modulo1/MyProgram/Program.cs
@@ -53,3 +53,58 @@ Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
 Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
+
+
+
+
+//NUMBERS OPERATIONS
+string firstname = "Bob";
+int widgetSold = 7;
+Console.WriteLine(firstname+" sold " + widgetSold + 7 + " widgets.");
+Console.WriteLine(firstname+" sold " + (widgetSold + 7 )+ " widgets.");
+
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+int module = 7 % 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+Console.WriteLine("Module: " + module);
+
+decimal decimalQuotinet = 7m / 5;
+Console.WriteLine(decimalQuotinet);
+
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+
+
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);
+
+int value1 = 1;
+
+Console.WriteLine("First: " + value1);
+Console.WriteLine($"Second: {value1++}");
+Console.WriteLine("Third: " + value1);
+Console.WriteLine("Fourth: " + (++value1));

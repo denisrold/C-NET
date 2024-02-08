842744605df73148c14716428a529f63b4d543f6
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-02-08 07:46:09 -0300

    bucles

---
+++ C#/CsharpProject/CodeBlocks/Program.cs
@@ -0,0 +1,145 @@
+﻿// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// bool found =false;
+// int total=0;
+// foreach (int number in numbers)
+// {
+//     total += number;
+//     if (number == 42)
+
+//         found = true;
+// }
+// if (found) 
+//         Console.WriteLine("Set contains 42");
+
+// Console.WriteLine($"Total: {total}");
+
+/////////////// SWITCH CASES
+
+
+
+// int employeeLevel =100;
+// string employeeName = "jhon Smith";
+// string title="";
+
+// switch(employeeLevel)
+// {  //case 100:
+//         // title="Junior Associate";
+//         // break;
+//     case 100:
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title= "Senior Manager";
+//         break;
+//     default:
+//         title= "Associate";
+//         break;
+// }
+
+// Console.WriteLine($"{employeeName},{title}");
+
+
+// SKU = Stock Keeping Unit. 
+
+// string sku = "01-MN-L";
+
+// string[] product = sku.Split('-');
+
+// string type = "";
+// string color = "";
+// string size = "";
+
+// switch(product[0]){ 
+// case "01":
+//     type = "Sweat shirt";
+//     break;
+// case "02":
+//     type = "T-Shirt";
+//     break;
+// case "03":
+//     type = "Sweat pants";
+//     break;
+// default:
+//     type = "Other";
+//     break;
+// }
+
+// switch(product[1]){ 
+// case "BL":
+//     color = "Black";
+//     break;
+// case "MN":
+//     color = "Maroon";
+//     break;
+// default:
+//     color = "White";
+//     break;
+// }
+
+// switch(product[2]){
+//  case "S":
+//     size = "Small";
+//     break;
+// case "M":
+//     size = "Medium";
+//     break;
+// case "L":
+//     size = "Large";
+//     break;
+// default:
+//     size = "One Size Fits All";
+//     break;
+//     }
+
+// Console.WriteLine($"Product: {size} {color} {type}");
+
+//BUCLE FOR 
+
+// for(int i=0;i <= 8;i++){
+//     Console.WriteLine(i);
+// }
+
+// for(int i = 10 ; i >0 ; i--){
+//     Console.WriteLine(i);
+// }
+
+
+// for(int i=0;i <= 9;i+=3){
+//     Console.WriteLine(i);
+//     if(i==6)break;
+// }
+// string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+// for(int i = names.Length -1; i >=0; i--){
+//     Console.WriteLine(names[i]);
+// }
+// for(int i = 0; i <= names.Length-1;i++)
+//     if(names[i] == "Eddie") 
+//         names[i] = "Sammy";
+
+// foreach(string name in names)
+//     Console.WriteLine(name);
+
+// // /*
+// Estas son las reglas de FizzBuzz que necesita implementar en el proyecto de código:
+
+// Valores de salida de 1 a 100, un número por línea, dentro del bloque de código de una instrucción de iteración.
+// Si el valor actual es divisible por 3, se imprime el término Fizz junto al número.
+// Si el valor actual es divisible por 5, se imprime el término Buzz junto al número.
+// Si el valor actual es divisible por 3 y por 5, se imprime el término FizzBuzz junto al número.
+
+
+for(int i = 1; i <=100;i++){
+    if(i % 3 == 0 && i % 5 == 0)
+        Console.WriteLine($"{i} FizzBuzz");
+    else if( i % 3 == 0)
+        Console.WriteLine($"{i} Fizz");
+    else if( i % 5 == 0)
+        Console.WriteLine($"{i} Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
--- C#/Games/dices/Program.cs
+++ C#/Games/dices/Program.cs
@@ -1,13 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*
-
-Si dos dados cualesquiera muestran el mismo valor, se obtienen dos puntos extra por obtener un doble.
-Si los tres dados muestran el mismo valor, se obtienen seis puntos extra por obtener un triple.
-Si la suma de los tres dados, junto con los puntos extra, es igual o mayor que 15, gana el juego. De lo contrario, pierde.
-A medida que obtenga más información sobre la instrucción if, podrá refinar las reglas.*/
-
-int scoreWin = 15;
-
+﻿int scoreToWin = 15;
 Random dice = new Random();
 int roll1 = dice.Next(1,7);
 int roll2 = dice.Next(1,7);
@@ -24,26 +15,26 @@ else{
 }
 
 int score = roll1+roll2+roll3+bonus;
-
 Console.WriteLine("\nPuntuacion final\n");
 Console.WriteLine($"dado 1: {roll1}");
 Console.WriteLine($"dado 2: {roll2}");
 Console.WriteLine($"dado 3: {roll3}");
+
 if(bonus == 2){
-Console.WriteLine("\nYou rolled doubles! +2 bonus to total!");
+    Console.WriteLine("\nYou rolled doubles! +2 bonus to total!");
 }
 if(bonus == 6){
-Console.WriteLine("\nYou rolled triples! +6 bonus to total!");
+    Console.WriteLine("\nYou rolled triples! +6 bonus to total!");
 }
 
 if(score>=15){
-Console.WriteLine($"\nFelicidades! su puntuacion es de: {score} usted ha GANADO un nuevo coche!\n");
+    Console.WriteLine($"\nFelicidades! su puntuacion es de: {score} usted ha GANADO un nuevo coche!\n");
 }
 else if(score>=10){
-Console.WriteLine($"\nFelicidades! su puntuacion es de: {score} usted ha GANADO una cantimplora!\n");
+    Console.WriteLine($"\nFelicidades! su puntuacion es de: {score} usted ha GANADO una cantimplora!\n");
 }
 else{
-Console.WriteLine($"\nLamentablemente ha perdido! Su puntuacion es de: {score} intentelo denuevo.\n");
+    Console.WriteLine($"\nLamentablemente ha perdido! Su puntuacion es de: {score} intentelo denuevo.\n");
 }
 
 

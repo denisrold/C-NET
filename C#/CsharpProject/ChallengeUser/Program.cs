6f17a91b1b5d08342974e63b286dec27c4effa46
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-02-09 09:17:46 -0300

    ReadResult ok

---
+++ C#/CsharpProject/ChallengeUser/Program.cs
@@ -0,0 +1,25 @@
+﻿string? readResult;
+// bool validEntry = false;
+// Console.WriteLine("Enter a string containing at least three characters:");
+// do{
+//   readResult = Console.ReadLine();
+//   if(readResult.Length >= 3 )
+//     validEntry = !validEntry;
+//   else
+//     Console.WriteLine("Your input is invalid, please try again.");
+        
+// }
+// while (validEntry == false);
+
+// Console.WriteLine($"This is the Final result: {readResult}");
+
+// numbers value
+int numberEntries = 0;
+bool numberValue = false;
+do{
+     readResult = Console.ReadLine();
+    numberValue = int.TryParse(readResult,out numberEntries);
+}
+while(numberValue == false);
+
+Console.WriteLine($"your number Result: {numberEntries}");
